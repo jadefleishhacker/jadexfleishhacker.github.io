a90a97af227fba50d2fdc9be481f2200dce4e201
Jade Fleishhacker <jadefleishhacker@gmail.com>
2021-03-08 04:17:11 -0800

    little details

--- myescapetheme/main.js
+++ myescapetheme/main.js
@@ -7,4 +7,16 @@ function closeNav() {
     document.getElementById("myNav").style.height = "0%";
 }
 
-
+$(document).ready(function () {
+    $("a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 1000, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+});
